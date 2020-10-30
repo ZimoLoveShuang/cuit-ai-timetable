b91d8044c13bad825d28cb29e501e3315a749aab
zimo <461009747@qq.com>
2020-10-30 20:40:33 +0800

    移除document，因为不支持

--- scheduleHtmlParser.js
+++ scheduleHtmlParser.js
@@ -1,97 +1,135 @@
-// 字符串转Table
-function strParseTable(arg) {
-    var objE = document.createElement("table");
-    objE.innerHTML = arg;
-    return objE;
-}
-
 function scheduleHtmlParser(html) {
-    // 每节课开始时间
-    var times = {
-        '1': '8:20',
-        '2': '9:15',
-        '3': '10:20',
-        '4': '11:15',
-        '5': '14:00',
-        '6': '14:55',
-        '8': '16:55',
-        '9': '19:30',
-        '10': '20:25'
-    };
+    String.prototype.replaceAll = function (s1, s2) {
+        return this.replace(new RegExp(s1, "gm"), s2);
+    }
+
+    // 每节课时间
+    var sectionTimes = [
+        {
+            "section": 1,
+            "startTime": "08:20",
+            "endTime": "09:05"
+        },
+        {
+            "section": 2,
+            "startTime": "09:15",
+            "endTime": "10:00"
+        },
+        {
+            "section": 3,
+            "startTime": "10:20",
+            "endTime": "11:05"
+        },
+        {
+            "section": 4,
+            "startTime": "11:15",
+            "endTime": "12:00"
+        },
+        {
+            "section": 5,
+            "startTime": "14:00",
+            "endTime": "14:45"
+        },
+        {
+            "section": 6,
+            "startTime": "14:55",
+            "endTime": "15:40"
+        },
+        {
+            "section": 7,
+            "startTime": "16:00",
+            "endTime": "16:45"
+        },
+        {
+            "section": 8,
+            "startTime": "16:55",
+            "endTime": "17:40"
+        },
+        {
+            "section": 9,
+            "startTime": "18:40",
+            "endTime": "19:25"
+        },
+        {
+            "section": 10,
+            "startTime": "19:35",
+            "endTime": "20:20"
+        },
+        {
+            "section": 11,
+            "startTime": "20:30",
+            "endTime": "21:15"
+        }
+    ];
 
     // 保存结果
     var result = [];
 
-    // 将字符串的表格转换为table对象
-    var table = strParseTable(html);
+    var table = $('table');
 
     // 遍历表格
     // i代表行，也就是第几节课，j代表列，也就是周几
-    for (var i = 0, rows = table.rows.length; i < rows; i++) {
-        for (var j = 0, cells = table.rows[i].cells.length; j < cells; j++) {
-            // 跳过表头
-            if (i == 0 || j == 0) {
-                continue;
-            }
-
-            // 两节小课一组，然后好像没有第11节课，先这样写，后期有了在优化
-            if (table.rows[i].cells[j].rowSpan != 1) {
-                var sp = table.rows[i].cells[j].innerHTML.split('<br>');
-                var nsp = [];
-                // 去掉sp中的空字符串，放到nsp数组中
-                for (var k = 0; k < sp.length; k++) {
-                    if (sp[k] != '') {
-                        nsp.push(sp[k].trim());
+    table.find('tr').each(function (i) {
+        $(this).find('td').each(function (j) {
+            if (i != 0 && j != 0) {
+                var info = unescape($(this).html().replace(/&#x/g, '%u').replace(/;/g, ''));
+                if (info.length > 0) {
+                    // console.log("周"+j+"第"+i+"节 "+info);
+                    var sp = info.split('<br>');
+                    var nsp = [];
+                    // 去掉sp中的空字符串，放到nsp数组中
+                    for (var k = 0; k < sp.length; k++) {
+                        if (sp[k] != '') {
+                            nsp.push(sp[k].replaceAll('</br>', '').trim());
+                        }
                     }
-                }
 
-                // 保存course信息
-                var course = {};
-                course['day'] = j;
-                var sections = [{'section': i, "startTime": times[i + ""], "endTime": ""}, {
-                    'section': i + 1,
-                    "startTime": times[(i + 1) + ""],
-                    "endTime": ""
-                }];
-                course['sections'] = sections;
+                    // 保存course信息
+                    var course = {};
+                    course['day'] = j;
+                    var sections = [];
+                    sections.push(sectionTimes[i - 1]);
+                    sections.push(sectionTimes[i]);
+                    course['sections'] = sections;
 
-                for (var k = 0; k < nsp.length; k++) {
-                    var value = nsp[k];
-                    switch ((k + 1) % 4) {
-                        case 1:
-                            course['name'] = value;
-                            break;
-                        case 2:
-                            course['teacher'] = value.substring('教师：'.length);
-                            break;
-                        case 3:
-                            var weekStr = value;
-                            weekStr = weekStr.substring("周次：".length, weekStr.length - "周".length);
-                            var weekStrSp = weekStr.split("-");
-                            var startWeek = parseInt(weekStrSp[0]);
-                            var endWeek = parseInt(weekStrSp[1]);
-                            var weeks = [];
-                            for (var w = startWeek; w <= endWeek; w++) {
-                                weeks.push(w);
-                            }
-                            course['weeks'] = weeks;
-                            break;
-                        case 0:
-                            course['position'] = value.substring('教室：'.length);
-                            result.push(course);
-                            course = {};
-                            course['day'] = j;
-                            sections = [{
-                                'section': i,
-                                "startTime": times[i + ""],
-                                "endTime": ""
-                            }, {'section': i + 1, "startTime": times[(i + 1) + ""], "endTime": ""}]
-                            course['sections'] = sections;
-                            break;
+                    for (var k = 0; k < nsp.length; k++) {
+                        var value = nsp[k];
+                        switch ((k + 1) % 4) {
+                            case 1:
+                                course['name'] = value;
+                                break;
+                            case 2:
+                                course['teacher'] = value.substring('教师：'.length);
+                                break;
+                            case 3:
+                                var weekStr = value;
+                                weekStr = weekStr.substring("周次：".length, weekStr.length - "周".length);
+                                var weekStrSp = weekStr.split("-");
+                                var startWeek = parseInt(weekStrSp[0]);
+                                var endWeek = parseInt(weekStrSp[1]);
+                                var weeks = [];
+                                for (var w = startWeek; w <= endWeek; w++) {
+                                    weeks.push(w);
+                                }
+                                course['weeks'] = weeks;
+                                break;
+                            case 0:
+                                course['position'] = value.substring('教室：'.length);
+                                result.push(course);
+                                course = {};
+                                course['day'] = j;
+                                sections = [];
+                                sections.push(sectionTimes[i - 1]);
+                                sections.push(sectionTimes[i]);
+                                break;
+                        }
                     }
                 }
             }
-        }
-    }
-    return {courseInfos: result}
+        });
+    });
+    return {
+        courseInfos: result,
+        sectionTimes: sectionTimes
+    };
 }
